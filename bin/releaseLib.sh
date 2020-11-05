41e623caa82dc8211c931f85a431b320916c07be
Naveed Yahyazadeh <naveed.yahyazadeh@appian.com>
2020-11-05 13:16:06 -0500

    Create new Gitlab job that releases new tags to Pypi

--- bin/releaseLib.sh
+++ bin/releaseLib.sh
@@ -1,28 +1,11 @@
 #!/bin/bash
 
-if [[ -z $1 ]]; then
-    echo "Please specify the pypi user you are trying to use"
-    exit 1
-else
-    PYPI_USER=$1
-    echo "Using PYPI_USER $PYPI_USER"
-fi
-
-if [[ $PYPI_USER == *"prod"* ]]; then
-    PYPI_LOCAL="perf-pypi-prod-local"
-    ROLE="perf-pypi-prod"
-else
-    PYPI_LOCAL="perf-pypi-dev-local"
-    ROLE="perf-pypi-dev"
-fi
-
 CURRENT_DIR=$(dirname "$0")
-source $CURRENT_DIR/releaseFunctions.sh
-echo "Using PYPI_LOCAL $PYPI_LOCAL"
 
-setup-pypirc
+pip install -U twine
 
-python ${CURRENT_DIR}/../setup.py sdist
+python ${CURRENT_DIR}/../setup.py sdist bdist_wheel
 
-# upload the tar.gz distribution created under dist directory to Artifactory using Twine
-pipenv run twine upload dist/* --repository ${PYPI_LOCAL}
+twine check dist/*
+
+twine upload dist/* -u $TWINE_USERNAME -p $TWINE_PASSWORD
