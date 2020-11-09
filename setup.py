17b6fc65f7512c9190b95d65a63704768d26fc97
Matthias Lee <matthias.lee@appian.com>
2020-11-09 16:06:29 -0500

    update setup.py to reference README.rst

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 from setuptools import setup, find_packages
 
 
-with open("README.md", "r") as fh:
+with open("README.rst", "r") as fh:
     long_description = fh.read()
 
 with open(os.path.join('appian_locust', 'VERSION')) as version_file:
@@ -14,7 +14,7 @@ setup(
     description='Tools and functions to make testing Appian with Locust easier',
     author='Appian Performance & Reliability Engineering Squad',
     long_description=long_description,
-    long_description_content_type="text/markdown",
+    long_description_content_type="text/x-rst",
     url="https://gitlab.com/appian-oss/appian-locust",
     packages=find_packages(exclude=["contrib",
                                     "docs",
